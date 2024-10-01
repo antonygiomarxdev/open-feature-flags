a870ee7a474b5fac6cc7c81fea0bb807ef90a15b
Antony Giomar Hernández <160435936+antonygiomarxdev@users.noreply.github.com>
2024-09-30 18:14:09 -0600

    style: standardize indentation and quotes in provider.registry.ts
    Updated indentation to 2 spaces and standardized quotes to double quotes across the file for consistency. These changes improve code readability and maintainability.

--- src/domain/providers/provider.registry.ts
+++ src/domain/providers/provider.registry.ts
@@ -1,27 +1,27 @@
-import { FeatureFlag } from '../entities/feature-flag.entity';
+import { FeatureFlag } from "../entities/feature-flag.entity";
 
 export interface CustomProvider {
-    loadFeatureFlags(): Promise<FeatureFlag[]>;
+  loadFeatureFlags(): Promise<FeatureFlag[]>;
 }
 
 export class ProviderRegistry {
-    private providers: { [key: string]: CustomProvider } = {};
+  private providers: { [key: string]: CustomProvider } = {};
 
-    registerProvider(name: string, provider: CustomProvider): void {
-        this.providers[name] = provider;
+  registerProvider(name: string, provider: CustomProvider): void {
+    this.providers[name] = provider;
+  }
+
+  async loadFeatureFlags(providerName: string): Promise<FeatureFlag[]> {
+    const provider = this.providers[providerName];
+
+    if (!provider) {
+      throw new Error(`Provider ${providerName} is not registered.`);
     }
 
-    async loadFeatureFlags(providerName: string): Promise<FeatureFlag[]> {
-        const provider = this.providers[providerName];
+    return provider.loadFeatureFlags();
+  }
 
-        if (!provider) {
-            throw new Error(`Provider ${providerName} is not registered.`);
-        }
-
-        return provider.loadFeatureFlags();
-    }
-
-    getAvailableProviders(): string[] {
-        return Object.keys(this.providers);
-    }
+  getAvailableProviders(): string[] {
+    return Object.keys(this.providers);
+  }
 }
